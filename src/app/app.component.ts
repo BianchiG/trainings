ff4665206918dfde50beeda7a441477f8b87f463
Bianca Golik <bianka.golik@gmail.com>
2021-06-23 17:17:51 +0200

    Add interalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trainings';
+  private currentLangs = ['en', 'pl'];
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang(this.currentLangs[0]);
+    translate.use(this.currentLangs[0]);
+  }
+
+  changeLang() {
+      this.translate.use(this.currentLangs.find(l => l != this.translate.currentLang) || this.translate.currentLang);
+    }
 }
